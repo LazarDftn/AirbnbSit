bcc0083900e20c11a1c4944fb97e9f57f0c3a197
cortan-SR25 <cortan.nebojsa@gmail.com>
2023-11-19 17:55:50 +0100

    Created user model on the frontend

---
+++ front-end-service/src/app/model/user.ts
@@ -0,0 +1,18 @@
+export class User{
+    firstName: string;
+    lastName: string;
+    username: string;
+    password: string;
+    email: string;
+    address: string;
+    type: string;
+    constructor(){
+        this.firstName = ""
+        this.lastName = ""
+        this.username = ""
+        this.password = ""
+        this.email = ""
+        this.address = ""
+        this.type = ""
+    }
+}

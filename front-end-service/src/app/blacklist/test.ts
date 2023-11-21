a3f3f5c5ba12957281068317e9de0cbf3d1df629
Sava Mihajlov <79896945+Savaxc@users.noreply.github.com>
2023-11-21 14:15:39 +0100

    Minor fix
    fix

---
+++ front-end-service/src/app/blacklist/test.ts
@@ -0,0 +1,5 @@
+// import * as fsPromise from 'fs/promises'; 
+// const file = await fsPromise.open('./words.txt', 'r');
+//  for await (const line of file.readLines()) {
+//      console.log(line);
+//  }
--- front-end-service/src/app/components/welcome-page/welcome-page.component.ts
+++ front-end-service/src/app/components/welcome-page/welcome-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'welcome-page',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome-page.component.css']
 })
 export class WelcomePageComponent {
-
+  
 }
+
+

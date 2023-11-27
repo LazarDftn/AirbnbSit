3e2a4f32bc7f2a934897e5d089b3390f70331fe2
Sava Mihajlov <79896945+Savaxc@users.noreply.github.com>
2023-11-27 17:13:59 +0100

    commit

--- front-end-service/src/app/components/welcome-page/welcome-page.component.ts
+++ front-end-service/src/app/components/welcome-page/welcome-page.component.ts
@@ -10,11 +10,5 @@ import { Component, Input } from '@angular/core';
 export class WelcomePageComponent {
   
 }
-let fruits: string[] = ['Apple', 'Orange', 'Banana']; 
-if (fruits.includes('Apples')) {
-  console.log('✅ The value is contained in array');
-} else {
-  console.log('⛔️ The value is NOT contained in array');
-}
-// console.log(fruits);
+
 

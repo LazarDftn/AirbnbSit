7ef2191e2e42b3ef255dc7ba30ff259b8b0289a2
cortan-SR25 <cortan.nebojsa@gmail.com>
2023-12-03 17:09:31 +0100

    Small fix with the AccomPrice json model

--- reservation-service/domain/accomPrice.go
+++ reservation-service/domain/accomPrice.go
@@ -2,6 +2,6 @@ package domain
 
 type AccommPrice struct {
 	AccommID string `json:"accommId"`
-	Price    int    `json:"percentage"`
+	Price    int    `json:"price"`
 	PayPer   int    `bson:"payPer" json:"payPer"` //nacin placanja, 0=ukupno, 1=po osobi
 }

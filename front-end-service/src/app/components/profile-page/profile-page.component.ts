b1e3400f6e025ab77b754e1b6f351e38713785fb
Sava Mihajlov <79896945+Savaxc@users.noreply.github.com>
2024-01-25 19:27:29 +0100

    Updated Profile Page

--- front-end-service/src/app/components/profile-page/profile-page.component.ts
+++ front-end-service/src/app/components/profile-page/profile-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-profile-page',
@@ -6,6 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile-page.component.css']
 })
 export class ProfilePageComponent {
+  
+  constructor(private authService: AuthService){}
+
+  // booleans for the navbar to check the users role and restrict access to pages
+  isUserLoggedIn = this.authService.userIsLoggedIn()
+  isUserHost = this.authService.userHasRole("HOST")
+  isUserGuest = this.authService.userHasRole("GUEST")
 
   username = localStorage.getItem("airbnbUsername")
 }
